734e2dfb6946dbe0125ca4982068c9cb0ca2c92f
PiyuCodere <piyushrai284@gmail.comm>
2024-03-27 19:51:16 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.write("App running!");
+});
+
 app.use("/api", adminRouter);
 
 app.listen(process.env.PORT, () =>
