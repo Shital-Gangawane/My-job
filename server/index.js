f651a319e1d922a965064c58c50b4c5023318d2b
PiyuCodere <piyushrai284@gmail.comm>
2024-03-30 19:31:11 +0530

    build

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use("/", express.static(path.join(__dirname, "build")));
 
 app.use("/api", adminRouter);
 
-app.get("/*", function (req, res) {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
