5fa71d77fde1d94183d72a1021efc1fbbeaec58c
Shital-Gangawane <114095926+Shital-Gangawane@users.noreply.github.com>
2024-04-24 20:37:41 +0530

    Add files via upload

--- client/src/pages/Employer/DashboardData/Messages.js
+++ client/src/pages/Employer/DashboardData/Messages.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Messages() {
   return (
-    <div>Messages</div>
+    <div className='mt-50'>
+      Messages
+    </div>
   )
 }
 
--- client/src/pages/Employer/DashboardData/Slidebar.js
+++ client/src/pages/Employer/DashboardData/Slidebar.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { FaRegUserCircle } from "react-icons/fa";
 
-function Slidebar() {
+
+function Slidebar({buttons, isSelected, setIsSelected}) {
+
   return (
-    <div className='flex flex-col bg-white w-72 items-center'>
-        <div className='flex gap-3 mb-3'>
+    <div className='flex'>
+    <div className='h-screen overflow-y-auto flex flex-col bg-white w-80 items-center'>
+        <div className='flex gap-3 mb-3 mt-4'>
             <FaRegUserCircle size={40}/>
-            <span className='gap-2 mt-2'>User Name</span> </div>
-           <div> <button className='login-btn w-full py-2 px-2 bg-[#6ad61d] text-white rounded-lg transition duration-300 ease-in-out'>View Profile</button></div>
+            <span className='gap-2 mt-2 font-bold'>User Name</span> </div>
+           <div> <button className=' w-full py-2 px-2 ms-auto bg-[#6ad61d] text-white rounded-lg transition duration-300 ease-in-out'>View Profile</button></div>
        
-        <div className='flex-1'></div>
-        <div className=''>Logout</div>
+        <div className='flex-1 mt-5 px-4 py-2 border-t'>
+          {
+            buttons.map((text, index)=>{
+              // w-full  py-3 ps-4 text-gray-400 text-left hover:bg-blue-100 hover:text-[#6ad61d] rounded-lg transition duration-300 ease-in-out
+              return  <button type="submit"
+              className= {`w-full  py-3 ps-4 text-gray-400 text-left hover:bg-blue-50 hover:text-[#6ad61d] rounded-lg `}
+              onClick={()=>setIsSelected(index)}
+              >
+              {text}
+            </button>
+            })
+          }
+        </div>
+        {/* <div className=''>Logout</div> */}
+        </div>
+          
         </div>
   )
 }
