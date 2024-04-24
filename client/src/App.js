4353d73d662575e7718c6c62010e4c71f8ffb7ca
Shital-Gangawane <114095926+Shital-Gangawane@users.noreply.github.com>
2024-04-24 20:40:57 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout.js/Layout";
 import Nav from "./components/Nav/Nav";
 import LandingPage from "./pages/LandingPage/Index";
+
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -24,6 +26,31 @@ import Job from "./pages/Job/Job";
 import ViewJob from "./pages/Job/ViewJob";
 import Employer from "./pages/Employer/Employer";
 import EmployerDetails from "./components/Admin/EmployerDetails";
+import Empdashboard from "./pages/Employer/Empdashboard";
+import Userdashboard from "./pages/Employer/DashboardData/Userdashboard";
+import Profile from "./pages/Employer/DashboardData/Profile";
+import Myjobs from "./pages/Employer/DashboardData/Myjobs";
+import Submitjobs from "./pages/Employer/DashboardData/Submitjobs";
+import Applicantsjobs from "./pages/Employer/DashboardData/Applicantsjobs";
+import Shortlistcandidate from "./pages/Employer/DashboardData/Shortlistcandidate";
+import Candidatealerts from "./pages/Employer/DashboardData/Candidatealerts";
+import Packages from "./pages/Employer/DashboardData/Packages";
+import Messages from "./pages/Employer/DashboardData/Messages";
+import Changepassword from "./pages/Employer/DashboardData/Changepassword";
+import Deleteprofile from "./pages/Employer/DashboardData/Deleteprofile";
+import Logout from "./pages/Employer/DashboardData/Logout";
+import Meeting from "./pages/Employer/DashboardData/Meeting";
+import Slidebar from "./pages/Employer/DashboardData/Slidebar";
+
+// import { useSelector } from "react-redux";
+// import { SelectUser } from "./features/userSlice";
+// import Logout from "./pages/Logout/Logout";
+
+// import Navcontents from "./components/Nav/Navcontents";
+// import Register from "./pages/Register/Register";
+// import Login from "./pages/Login/Login";
+// import Home from "./components/home/Home";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -41,6 +68,7 @@ const router = createBrowserRouter(
         <Route path="candidates" element={<CandidateModule />} />
         <Route path="employers" element={<Employers />}>
           <Route path=":_id" element={<EmployerDetails />} />
+          
         </Route>
         <Route path="jobs" element={<Jobs />} />
         <Route path="admins" element={<AdminModule />} />
@@ -50,13 +78,52 @@ const router = createBrowserRouter(
       </Route>
       <Route path="employer" element={<Employer />}>
         <Route path="dashboard" element={<CandidateDashboard />} />
+        <Route path="empdashboard" element={<Empdashboard />} />
       </Route>
+      <Route>
+      <Route path="empdashboard" element={<Empdashboard />} />
+      <Route path="slidebar" element={<Slidebar />} />
+      <Route path="userdashboard" element={<Userdashboard />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="mujobs" element={<Myjobs />} />
+      <Route path="submitjobs" element={<Submitjobs />} />
+      <Route path="applicantjobs" element={<Applicantsjobs />} />
+      <Route path="shortlistcandidate" element={<Shortlistcandidate />} />
+      <Route path="candidatealerts" element={<Candidatealerts />} />
+      <Route path="packages" element={<Packages />} />
+      <Route path="messages" element={<Messages />} />
+      <Route path="meeting" element={<Meeting />} />
+      <Route path="changepassword" element={<Changepassword />} />
+      <Route path="deleteprofile" element={<Deleteprofile />} />
+      <Route path="logout" element={<Logout />} />
+      </Route>
+
     </Route>
+   
   )
 );
 
 function App() {
-  return <RouterProvider router={router} />;
+  // const user=useSelector(SelectUser)
+  // return(
+  //   <div>
+  //     <Navcontents/>
+  //     <BrowserRouter>
+  //     <Routes>
+  //       <Route path="/" element={<Home/>}></Route>
+  //       <Route path="/Register" element={<Register />}></Route>
+  //         <Route path="/Login" element={<Login />}></Route>
+  //     </Routes>
+  //     </BrowserRouter>
+  //   </div>
+  // )
+
+  return(
+  <RouterProvider router={router} />
+ 
+
+) 
+   
 }
 
 export default App;
