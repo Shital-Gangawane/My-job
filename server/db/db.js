cb61fb4f7fe38aeef55c8774e40d833072a129ba
PiyuCodere <piyushrai284@gmail.comm>
2024-04-30 12:07:59 +0530

    Revert "updated db.js"
    This reverts commit 847c8accf9ba8ff95fb03d5a02a75d54c77bbc14.

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports.connectDb = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ulebre1.mongodb.net/jobportal?retryWrites=true&w=majority&appName=Cluster0`
-    );
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
