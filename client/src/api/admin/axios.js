6126383a6d36ae31b18a51dad9bfb1876a813d51
PiyuCodere <piyushrai284@gmail.comm>
2024-04-30 08:17:48 +0530

    build htaccess

--- client/src/api/admin/axios.js
+++ client/src/api/admin/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = process.env.REACT_APP_SERVER_API_URL;
+const url = process.env.REACT_APP_SERVER_API_URL || "http://localhost:8000";
 
 // const url = "http://localhost:8000";
 
