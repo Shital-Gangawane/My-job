847c8accf9ba8ff95fb03d5a02a75d54c77bbc14
PiyuCodere <piyushrai284@gmail.comm>
2024-04-30 09:29:13 +0530

    updated db.js

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 module.exports.connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ulebre1.mongodb.net/jobportal?retryWrites=true&w=majority&appName=Cluster0`
+    );
     console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
