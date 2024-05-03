bcde6c444295fadd83592f829bd5ffcde303da77
PiyuCodere <piyushrai284@gmail.comm>
2024-05-03 14:41:03 +0530

    mongoose createConnection

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const { connectDb } = require("./db/db.js");
 const adminRouter = require("./routes/admin/admin.routes.js");
 const candidateRouter = require("./routes/candidate/candidate.routes.js");
 const employerRouter = require("./routes/employer/employer.routes.js");
-const { Admin } = require("./models/admin/admin.js"); // Import Admin model
+
+connectDb();
+require("./models/admin/admin.js"); // Import Admin model
 
 const app = express();
 
@@ -32,14 +34,16 @@ console.log("PORT:", process.env.PORT);
 const PORT = process.env.PORT || 8000;
 
 // Connect to the database before starting the server
-connectDb()
-  .then(() => {
-    // Database connection successful, start the server
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    // Database connection failed, log the error
-    console.error("Failed to connect to database:", err);
-  });
+// connectDb()
+//   .then(() => {
+//     // Database connection successful, start the server
+
+//   })
+//   .catch((err) => {
+//     // Database connection failed, log the error
+//     console.error("Failed to connect to database:", err);
+//   });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
