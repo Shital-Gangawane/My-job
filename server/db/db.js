938caff9f73aa9f45b4a429267cda13310704fe2
PiyuCodere <piyushrai284@gmail.comm>
2024-05-03 11:37:31 +0530

    db connection string

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
-const { adminSchema } = require("../models/admin/admin.js");
 
 module.exports.connectDb = async () => {
   await mongoose
-    .connect(
-      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ulebre1.mongodb.net/jobportal?retryWrites=true&w=majority&appName=Cluster0`
-    )
+    .connect(process.env.MONGODB_URL)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.log(err));
 };
