55a290d41a1c2faafa42e8819338149e820c0d2e
PiyuCodere <piyushrai284@gmail.comm>
2024-05-19 21:35:01 +0530

    firebaseConfig secrets

--- client/src/utils/firebaseConfig.js
+++ client/src/utils/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBODekWsobUY_T0e9aaAwR2NbdZQdNF2kc",
-  authDomain: "projob-13ccd.firebaseapp.com",
-  projectId: "projob-13ccd",
-  storageBucket: "projob-13ccd.appspot.com",
-  messagingSenderId: "991601853348",
-  appId: "1:991601853348:web:03d97fd890a5c5cc823731",
-  measurementId: "G-XN44HBH253",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
